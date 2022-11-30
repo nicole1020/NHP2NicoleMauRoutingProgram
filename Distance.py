0ae60dcecca967d8dd9d1c8cbb4a6a95dcd50cc3
nicole1020 <annabellesmom1>
2022-11-29 16:11:15 -0800

    11.29 4th

--- Distance.py
+++ Distance.py
@@ -1,4 +1,5 @@
 import csv
+from csv import reader
 
 
 class Distance:
@@ -9,12 +10,15 @@ class Distance:
         return " %s " % self.distance
 
 
-distanceData = []
+with open('Distance.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    # Pass reader object to list() to get a list of lists
+    distanceData = list(csv_reader)
+    print(distanceData)
 
-with open('Distance.csv') as csvfile:
-    csvReader = csv.reader(csvfile)
-    for row in csvReader:
-        distanceData.append(row[0])
-
-print(distanceData)
 # 410 S State St., Salt Lake City, UT 84111
+row_number = 3
+col_number = 2
+value = distanceData[row_number - 1][col_number - 1]
+print('Value in cell at 3rd row and 2nd column : ', value)
